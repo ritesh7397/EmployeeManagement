26e1e692c76d1e78104ce98257d876adbde91071
Ritesh Nagrikar <ritesh@gmail.com>
2026-08-27 17:54:28 +0530

    Removed comment line

--- Employee.UI/Controllers/EmployeeController.cs
+++ Employee.UI/Controllers/EmployeeController.cs
@@ -77,7 +77,6 @@ namespace Employee.UI.Controllers
             var model = new EmployeeDetailViewModel
             {
                 //List = employees.OrderBy(e => e.EmployeeName).ToList(),
-                // List = employees.OrderByDescending(e => e.EmployeeName).ToList(),
                 List = employees.OrderByDescending(e => e.EmployeeName).ToList(),
                 Employee = new Employees()
 
