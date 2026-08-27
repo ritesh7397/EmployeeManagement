6034614fa4066620f58b9bb8b013e65a79f1c4fd
Ritesh Nagrikar <ritesh@gmail.com>
2026-08-27 14:53:43 +0530

    Add employee validation

--- Employee.Domain/Entity/Employees.cs
+++ Employee.Domain/Entity/Employees.cs
@@ -1,5 +1,4 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Employee.Domain.Entity
 {
@@ -7,8 +6,14 @@ namespace Employee.Domain.Entity
     {
         [Key]
         public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string EmployeeName { get; set; }
+
+        [Range(1, 1000000)]
         public int EmployeeSalary { get; set; }
+
         public string? EmployeeDescription { get; set; }
     }
 }
